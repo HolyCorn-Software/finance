317a0f19c77438919acc22d0298402af07636b8b
Son of Binary <akwotom@hcts.ml>
2022-11-08 17:58:54 +0100

    Added methods that help other faculties make use of the faculty of finance

--- finance-changes.md
+++ finance-changes.md
@@ -1,5 +1,3 @@
 
-    Removed unnecessary logging
-
-    Corrected race condition bug, that led to "loss" of money, by serializing execution of payment records.
-    The bug is simple: If the execute method is called twice, it is likely that two external transactions are created, and the client pays for the one which the system doesn't know about
+    
+Added methods that help other faculties make use of the faculty of finance
--- payment/terminals/internal.mjs
+++ payment/terminals/internal.mjs
@@ -44,5 +44,79 @@ export default class PaymentInternalMethods {
         return await this[controller_symbol].findRecord({ ...arguments[1] })
     }
 
+    /**
+     * This method returns information about all the payment methods
+     * @returns {Promise<import("../types.js").ProviderPaymentMethodsInfo>}
+     */
+    async getPaymentMethods() {
+        return await this[controller_symbol].getPaymentMethods()
+    }
+
+
+
+
+    /**
+     * This method is used by a client to update a record
+     * @param {object} param0 
+     * @param {string} param0.id
+     * @param {import("../types.js").PaymentWritablePublicData} param0.data
+     * @returns {Promise<void>}
+     */
+    async publicUpdate({ id, data }) {
+        id = arguments[1]?.id
+        data = arguments[1]?.data
+        await this[controller_symbol].publicUpdate({ id, data })
+    }
+
+    async userDismiss({ id }) {
+        return await this[controller_symbol].userDismiss({ id: arguments[1]?.id })
+    }
+
+
+    /**
+     * This returns the list of all payment methods
+     * @returns {Promise<[import("../types.js").ClientPaymentMethodInfo]>}
+     */
+    async getPaymentMethods() {
+
+        let methods = await this[controller_symbol].getPaymentMethods();
+
+        //Simply convert the buffers into data urls
+        return methods.map(method => {
+            method.image.mimeType ||= 'image/png'
+            let new_method = { ...method, image: { mimeType: method.image.mimeType } }
+            new_method.image.data = `data:${method.image.mimeType};base64,${method.image.data.toString('base64')}`
+            return new_method
+        })
+
+    }
+
+
+    /**
+     * This method is used by the client to retrieve the form used by the client for a particular purpose
+     * @param {object} param0 
+     * @param {('invoice'|'payout')} param0.intent
+     * @param {string} param0.method
+     * @returns {Promise<import("/$/system/static/html-hc/widgets/multi-flex-form/types.js").MultiFlexFormDefinitionData}
+     */
+    async getInlineForm({ intent, method }) {
+        return await this[controller_symbol].getInlineForm({ ...arguments[1] })
+    }
+
+
+    /**
+     * This method is used to execute a payment
+     * @param {object} param0 
+     * @param {string} param0.id
+     * @returns {Promise<void>}
+     */
+    async execute({ id }) {
+
+        checkArgs(arguments[1], { id: 'string' }, 'input')
+
+        await this[controller_symbol].execute({ id: arguments[1]?.id })
+    }
+
+
 
 }
